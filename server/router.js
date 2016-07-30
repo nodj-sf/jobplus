adbad2a0383a1d06552dd80eb3ebe1d8219ef77b
Justin Lien <lien.justin.m@gmail.com>
2016-07-30 11:30:09 -0700

    config: add .env

--- server/router.js
+++ server/router.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+/*
+** Load local environment variables from .env 
+** file where secrets and keys are configured.
+*/
+let dotenv;
+if (!process.env.SESSION_SECRET) {
+  dotenv = require('dotenv');
+  dotenv.load({ path: '.env' });
+}
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 module.exports = app;
