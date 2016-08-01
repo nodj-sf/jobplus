71ba48b2d0542bd38f1669a0a62118947064b03f
Oliver Isenrich <IsenrichO@gmail.com>
2016-08-01 15:39:53 -0700

    Added personal stylesheet

---
+++ app/components/job_list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import JobListItem from './job_list_item';
+
+const JobList = (props) => {
+  const jobItems = props.jobs.map((job) => {
+    return <JobListItem key={job.id} job={job} />; 
+  });
+  
+  return (
+    <ul className="jobsUL">
+      {jobItems}
+    </ul>
+  );
+};
+
+export default JobList;
