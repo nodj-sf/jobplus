ec217f48be23bb7275083f69fb9a0f17940c2471
nick_d <ngdaniele@gmail.com>
2016-08-01 11:16:57 -0700

    Fixed merge conflict

--- app/app.js
+++ app/app.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './home';
 
-class App extends Component {
+import SearchBar from './components/search_bar.jsx';
+import JobList from './components/job_list.jsx';
+// Need to import api function or figure out how to make own....
+const API_Key = 'dsflkjhaerthag';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Dummy data put in for now
+    this.state = { jobs: [{title: 'Front-End', id: '1'}, {title: 'Back-End', id: '2'}, {title: 'Full-Stack', id: '3'}] };
+
+    // Uncomment when api figured out. first call for data
+    // this.jobSearch('programmer');
+  }
+
+  jobSearch(term) { 
+    console.log(term);
+    // Need to use API Here....
+    // GLASSDOORSearch({key: API_KEY, term: term}, (jobs) => {
+    //   this.setState({
+    //     jobs: jobs
+    //   });
+    // });
+  }
+
   render() {
     return (
       <div>
-        <h1>Our App</h1>
-        <Home />
+        <SearchBar onSearchTermChange={term => this.jobSearch(term)}/>
+        <JobList jobs={this.state.jobs} />
       </div>
     );
   }
-}
-
-export default App;
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
