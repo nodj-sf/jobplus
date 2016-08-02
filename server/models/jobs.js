6764b624cf2a09df0586c1d6a4afbf2f5eee935c
Damon <damonnguyen92@gmail.com>
2016-08-02 12:03:32 -0700

    Create model for querying jobs

--- server/models/jobs.js
+++ server/models/jobs.js
@@ -1,5 +1,20 @@
 'use strict';
-
+const request = require('request');
 const jobs = require('../externalAPI/indeed.json');
 
-module.exports = jobs;
+let getIndeed = (callback, query, location, userIP) => {
+  query = encodeURI(query);
+  location = encodeURI(location);
+  return (res) => {
+    request('http://api.indeed.com/ads/apisearch?format=json&publisher=' + 
+      process.env.INDEED + '&q=' + 
+      query + '&l=' + 
+      location + '&radius=.5&st=jobsite&jt=fulltime&limit=15&filter=1&latlong=1&co=us&userip=' +
+      userIP + '&v=2', 
+      function(error, response, body) {
+        return res.send(body);
+      });
+  }
+};
+
+module.exports = getIndeed;
