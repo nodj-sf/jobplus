73d829d0aeacdf1fe3be917bb2855b2b0d8a4bd6
Damon <damonnguyen92@gmail.com>
2016-08-02 13:35:59 -0700

    Input ip address as argument to Indeed search API

--- server/controllers/getIndeed.js
+++ server/controllers/getIndeed.js
@@ -2,12 +2,12 @@ const request = require('request');
 const getIndeed = require('../models/jobs');
 
 exports.get = (req, res) => {
-  // var ip = req.headers['x-forwarded-for'] || 
-  // req.connection.remoteAddress || 
-  // req.socket.remoteAddress ||
-  // req.connection.socket.remoteAddress;
+  var ip = req.headers['x-forwarded-for'] || 
+  req.connection.remoteAddress || 
+  req.socket.remoteAddress ||
+  req.connection.socket.remoteAddress;
   
-  let data = getIndeed(null,'Software engineer', 'San Francisco', '173.247.204.106')(res);
+  let data = getIndeed(null,'Software engineer', 'San Francisco', ip)(res);
   return data;
   res.end();
 }
