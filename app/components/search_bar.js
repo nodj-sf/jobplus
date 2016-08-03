197d62c9dfb52821471486fe2dd37a40c0e2cae9
Oliver Isenrich <IsenrichO@gmail.com>
2016-08-03 12:09:33 -0700

    Tweaks

--- app/components/search_bar.js
+++ app/components/search_bar.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchJobs } from '../actions/index';
-=======
->>>>>>> b5e0203ec8c6751d0a9207f448a524c8689df22d
+
 
 class SearchBar extends Component {
   constructor(props) {
@@ -29,7 +27,6 @@ class SearchBar extends Component {
   render() {
     return (
       <form onSubmit={this.onFormSubmit}>
-<<<<<<< HEAD
         <div className="box">
           <div className="container-3">
               <span className="icon"><i className="fa fa-search"></i></span>
@@ -40,13 +37,6 @@ class SearchBar extends Component {
                 autoSave="Developer Jobs" 
                 placeholder="Search..."
                 onChange={this.onInputChange} />
-=======
-
-        <div className="box">
-          <div className="container-3">
-              <span className="icon"><i className="fa fa-search"></i></span>
-              <input id="search" type="search" results="4" autoSave="Developer Jobs" placeholder="Search..." />
->>>>>>> b5e0203ec8c6751d0a9207f448a524c8689df22d
           </div>
         </div>
 
@@ -56,24 +46,9 @@ class SearchBar extends Component {
   }
 };
 
-<<<<<<< HEAD
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchJobs }, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(SearchBar);
-=======
-export default SearchBar;
-
-        // <input 
-        //   placeholder="Search for a job"
-
-        //   type="search"
-        //   className="searchBar"
-
-        //   type="search"
-        //   className="searchBar"
-        //   value={ this.state.term }
-        //   onChange={ this.onInputChange } />
->>>>>>> b5e0203ec8c6751d0a9207f448a524c8689df22d
