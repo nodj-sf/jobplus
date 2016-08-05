a7691017fb3fdb3751589ca89f6dfc05b2b0d113
nick_d <ngdaniele@gmail.com>
2016-08-05 13:22:53 -0700

    feat(routing): configured application routing

---
+++ app/components/landing_page.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar';
+
+
+class LandingPage extends Component {
+ render() {
+   return (
+     <div>
+       <h1>Landing Page</h1>
+       <SearchBar />
+     </div>
+   );
+ }
+
+}
+
+export default LandingPage;
