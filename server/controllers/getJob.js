24628c4bdb08d7efee69c11d1bb956448006ea5b
Justin Lien <lien.justin.m@gmail.com>
2016-08-05 11:34:42 -0700

    feat(redis): add caching for indeed & yelp

--- server/controllers/getJob.js
+++ server/controllers/getJob.js
@@ -1,17 +1,54 @@
 const request = require('request');
 const getIndeed = require('../models/jobs');
+const redisClient = require('redis').createClient;
+const redis = redisClient(6379, 'localhost');
 
 exports.post = (req, res) => {
   let jobTitle = req.body.jobTitle,
       city = req.body.city;
+  let key = JSON.stringify(req.body);
 
-  let ip = req.headers['x-forwarded-for'] || 
-      req.connection.remoteAddress || 
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
+  // redis.del(key);
 
-  let data = getIndeed(jobTitle, city, ip)(res);
-  res.setHeader('Content-Type', 'application/json');
-  return data;
-  res.end();
+  /*
+   * Return data from cache if exists
+  */
+  redis.get(key, (err, result) => {
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if (result) {
+      // console.log('return from redis');
+      res.send(JSON.parse(result));
+      res.end();
+    } else {
+      let ip = req.headers['x-forwarded-for']
+            || req.connection.remoteAddress
+            || req.socket.remoteAddress
+            || req.connection.socket.remoteAddress;
+      
+      // console.log('make api call');
+
+      /*
+       * Search Indeed API and cache data
+       * @param {string} jobTitle 
+       * @param {string} city
+       * @param {string} ip
+       * @return response JSON || response from cache
+      */
+      getIndeed(jobTitle, city, ip)(res)
+        // Return data when a promise is return.
+        .then((res) => {
+          // Cache data using request body as key
+          redis.set(key, res.data);
+          // Set cache to expire in an hour
+          redis.expire(key, 3600);
+          res.respond;
+          res.end();
+        }, (err) => {
+          console.log('err: ', err);
+          res.end();
+        });
+    }
+  });
 }
