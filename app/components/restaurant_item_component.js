daa26e7e032568b156dd2ed9e3775423fab65a72
Damon <damonnguyen92@gmail.com>
2016-08-06 15:51:15 -0700

    refactor: Remove unneccessary co

--- app/components/restaurant_item_component.js
+++ app/components/restaurant_item_component.js
@@ -4,8 +4,7 @@ class RestaurantListItem extends Component {
 
   render() {
     return (
-      <li className="jobLI" onClick={() => selectJob(this.props.job)}>
-        <h2>{this.state.job.jobtitle}</h2> 
+      <li className="restaurantLI"}>
       </li>
     );
   }
@@ -13,13 +12,3 @@ class RestaurantListItem extends Component {
 
 export default RestaurantListItem;
 
-/*
-{
-  name: 'pseudoRestaurant',
-  url: 'www.pesudo.com',
-  rating: 5,
-  review_count: 999,
-  phone: 123-456-7891,
-  coordinate: null
-}
-*/
