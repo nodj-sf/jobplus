b6d8945a06f49cf51ddc0d775ea29901a915fb48
Damon <damonnguyen92@gmail.com>
2016-08-06 14:07:21 -0700

    Feat: Create constant variable for type action

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,7 +1,10 @@
+const FETCH_JOBS = 'FETCH_JOBS';
+const JOB_SELECTED = 'JOB_SELECTED';
+
 export const fetchJobs = (jobData) => {
   console.log('Redux Action:', jobData);
   return {
-    type: 'FETCH_JOBS',
+    type: FETCH_JOBS,
     payload: jobData
   };
 }
@@ -9,7 +12,7 @@ export const fetchJobs = (jobData) => {
 export const selectJob = (job) => {
   console.log('A job has been selected:', job);
   return {
-    type: 'JOB_SELECTED',
+    type: JOB_SELECTED,
     payload: job
   };
 }
