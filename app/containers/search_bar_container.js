dfd6f0e70d79182d92661d4a8c821a3d32fd4bc4
Justin Lien <lien.justin.m@gmail.com>
2016-08-07 14:22:01 -0700

    feat: make use of react-reduce middware
    - assign promise to actions and have middleware handle the return
    promise

--- app/containers/search_bar_container.js
+++ app/containers/search_bar_container.js
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import { Router, Route, hashHistory } from 'react-router';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import axios from 'axios';
 
 import { fetchJobs } from '../actions/index';
 
@@ -18,7 +17,6 @@ class SearchBar extends Component {
     this.onJobTitleInputChange = this.onJobTitleInputChange.bind(this);
     this.onLocationInputChange = this.onLocationInputChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.searchJob = this.searchJob.bind(this);
   }
 
   onJobTitleInputChange(event) {
@@ -31,22 +29,10 @@ class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
-    this.searchJob(this.state.jobTerm, this.state.locationTerm);
+    this.props.fetchJobs(this.state.jobTerm, this.state.locationTerm);
+    hashHistory.push('results');
     this.setState({ jobTerm: '' });
     this.setState({ locationTerm: '' });
-  }
-  
-  searchJob(jobTerm, locationTerm) {
-    console.log('logging in searchJob');
-    axios.post('/api/v1/jobs', {
-      jobTitle: jobTerm,
-      city: locationTerm // Hardcoded
-    }).then(function(response) {
-      this.props.fetchJobs(response.data.results);
-      hashHistory.push('results');
-    }.bind(this)).catch(function(error) {
-      console.log(error);
-    });
   }
 
   render() {
