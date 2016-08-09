6779ca7bc4d07f18ba4914b452c85ca06d04d901
nick_d <ngdaniele@gmail.com>
2016-08-09 16:02:44 -0700

    feat(config): Configured files for new yelp api call

--- app/containers/job_list_container.js
+++ app/containers/job_list_container.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { selectJob } from '../actions/index';
+import { selectJob, fetchYelp } from '../actions/index';
 import JobItem from '../components/job_item_component';
 
 
 class JobList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.jobFunc = this.jobFunc.bind(this);
+  }
+
+  jobFunc(job) {
+    this.props.selectJob(job);
+    this.props.fetchYelp(job.latitude, job.longitude);
+  }
+
   renderList() {
     return this.props.jobs.map((job) => {
       return (
         <JobItem
           key={job.jobkey}
-          jobFunc={this.props.selectJob}
+          jobFunc={this.jobFunc}
           job={job} />
       );
     });
@@ -41,7 +52,7 @@ function mapStateToProps(state) {
 // notify all containers of the action that can be triggered
 function mapDispatchToProps(dispatch) {
   // Whenever loadJobs is called, the result should be passed to all reducers
-  return bindActionCreators({ selectJob: selectJob }, dispatch)
+  return bindActionCreators({ selectJob: selectJob , fetchYelp: fetchYelp}, dispatch)
 }
 
 // Promote JobList to a container
--- app/reducers/reducer_active_job.js
+++ app/reducers/reducer_active_job.js
@@ -1,4 +1,5 @@
 export default function(state = null, action) {
+  console.log("action", action.payload);
   switch(action.type) {
     case 'JOB_SELECTED':
       return action.payload;
