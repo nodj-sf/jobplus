488c75465f4fa8c2950996c42a33fbb3026d03e7
Damon <damonnguyen92@gmail.com>
2016-08-10 13:51:11 -0700

    Feat(init): Render information about a restaurant

--- app/components/restaurant_item_component.js
+++ app/components/restaurant_item_component.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 class RestaurantListItem extends Component {
   render() {
+    console.log('this.props.restaurant: ', this.props.restaurant);
+    var starRatingImage = this.props.restaurant.rating_img_url;
+    console.log('starRatingImage: ', starRatingImage);
     return (
-      <li className="restaurantLI">
-        <h1>{ this.props.restaurant.name }</h1>
+      <li className="restaurantLI" style={{"list-style-type": "none"}} >
+        <div className="nameRating">
+          <a style={{"textDecoration": "none"}} target="_blank" href={ this.props.restaurant.url }>{ this.props.restaurant.name }</a>
+          <img src={ this.props.restaurant.rating_img_url } /> { this.props.restaurant.review_count } reviews
+        </div>
+        <div className="yelpDesctiption">
+          <p>{ this.props.restaurant.display_address[1] } </p>
+          <p>{ this.props.restaurant.display_address[0] } </p>
+          <p>{ this.props.restaurant.display_address[2] } </p>
+          <p>{ this.props.restaurant.phone.replace(/^(\d{3})(\d{4})(\d{3})/, "+1 ($1) $2-$3") } </p>
+        </div>
       </li>
     );
   }
