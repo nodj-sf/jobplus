319d4ce615f0c52caa0cca73f5f9e7efb3ed0304
Oliver Isenrich <IsenrichO@gmail.com>
2016-08-12 16:56:31 -0700

    chore: Refactor

--- app/components/google_map_component.js
+++ app/components/google_map_component.js
@@ -124,7 +124,6 @@ class GMap extends BaseComponent {
             data-formattedLocation={marker.formattedLocation}
             position={ new google.maps.LatLng(marker.coords) }
             // position={ marker.coords }
-            position={marker.coords}
             animation={google.maps.Animation.DROP}
             title={marker.company}
             opacity={0.90}
@@ -175,6 +174,7 @@ let mapStateToProps = (state) => ({
 let mapStateToProps = (state) => ({
   markers: state.jobs.map(job => ({ 
     coords: new google.maps.LatLng(job.latitude, job.longitude),
+    coords: { "lat": job.latitude, "lng": job.longitude },
     jobTitle: job.jobtitle,
     company: job.company, 
     formattedLocation: job.formattedLocation,
