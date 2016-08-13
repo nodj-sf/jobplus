53ae2dae7feb45d941b9e814fc9d8f0c527c81d5
Justin Lien <lien.justin.m@gmail.com>
2016-08-13 12:34:12 -0700

    feat: pass csrf to server

--- public/js/main.js
+++ public/js/main.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "43054056d2e658798c78"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "95edaa20187f229e85ce"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -38370,10 +38370,20 @@
 	var FETCH_YELP = 'FETCH_YELP';
 	var FETCH_GPLACES = 'FETCH_GPLACES';
 	
+	function getCookie(name) {
+	  var value = "; " + document.cookie;
+	  var parts = value.split("; " + name + "=");
+	  if (parts.length === 2) {
+	    return decodeURIComponent(parts.pop().split(";").shift());
+	  }
+	  return '';
+	}
+	
 	var fetchJobs = exports.fetchJobs = function fetchJobs(jobSearch, city) {
 	  var request = _axios2.default.post('/api/v1/jobs', {
 	    jobTitle: jobSearch,
-	    city: city
+	    city: city,
+	    _csrf: getCookie('_csrf')
 	  });
 	
 	  return {
@@ -38388,7 +38398,8 @@
 	    coordinate: {
 	      latitude: lat,
 	      longitude: long
-	    }
+	    },
+	    _csrf: getCookie('_csrf')
 	  });
 	  return {
 	    type: FETCH_YELP,
@@ -38401,7 +38412,8 @@
 	    coordinate: {
 	      lat: lat,
 	      long: long
-	    }
+	    },
+	    _csrf: getCookie('_csrf')
 	  });
 	  return {
 	    type: FETCH_GPLACES,
