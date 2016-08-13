b9c77682d526d6a0184efee8a644b729ea502680
Oliver Isenrich <IsenrichO@gmail.com>
2016-08-12 22:45:15 -0700

    Implemented flex-box layout in Yelp container as well as selectively applied responsive text sizing

--- app/components/restaurant_item_component.js
+++ app/components/restaurant_item_component.js
@@ -12,16 +12,18 @@ class RestaurantListItem extends Component {
     var starRatingImage = this.props.restaurant.rating_img_url;
     return (
       <li className="restaurantLI" >
-        <div className="nameRating">
-          <a target="_blank" href={ this.props.restaurant.url }>{ this.props.restaurant.name }</a>
-          <img src={ this.props.restaurant.rating_img_url } /> 
-          <p className="numRestaurantReviews">{`${this.props.restaurant.review_count} Reviews`}</p>
-        </div>
-        <div className="yelpDescription">
-          <p>{ this.props.restaurant.display_address[1] } </p>
-          <p>{ this.props.restaurant.display_address[0] } </p>
-          <p>{ this.props.restaurant.display_address[2] } </p>
-          <p>{ this.parsePhoneNumber(this.props.restaurant.phone) }</p>
+        <div className="verticallyCenter">
+          <div className="nameRating">
+            <a target="_blank" href={ this.props.restaurant.url }>{ this.props.restaurant.name }</a>
+            <img src={ this.props.restaurant.rating_img_url } /> 
+            <p className="numRestaurantReviews">{`${this.props.restaurant.review_count} Reviews`}</p>
+          </div>
+          <div className="yelpDescription">
+            <p>{ this.props.restaurant.display_address[1] } </p>
+            <p>{ this.props.restaurant.display_address[0] } </p>
+            <p>{ this.props.restaurant.display_address[2] } </p>
+            <p>{ this.parsePhoneNumber(this.props.restaurant.phone) }</p>
+          </div>
         </div>
       </li>
     );
