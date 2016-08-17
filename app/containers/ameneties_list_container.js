4a87f6e10d565ec7f8044779c8782c46919f1c22
nick_d <ngdaniele@gmail.com>
2016-08-17 10:13:50 -0700

    feat(places): Places now display multiple results

--- app/containers/ameneties_list_container.js
+++ app/containers/ameneties_list_container.js
@@ -1,50 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-
 class AmenetiesList extends Component {
-
-  customList(parks, gyms) {
-    var topResults = [parks[0], gyms[0]];
-    return topResults;
-  }
-
-  renderList() {
-    var newList = this.customList(this.props.activeParks, this.props.activeGyms);
-    return (
-      <div>
-        <li className="placesLI" 
-            style={{"listStyleType": "none"}}
-            key={newList[0].place_id} >
-          <h4>Parks & Recreation</h4>  
-          <h4>{ newList[0].name }</h4>
-        </li>
-        <li className="placesLI" 
-            style={{"listStyleType": "none"}}
-            key={newList[1].place_id} >
-          <h4>Gyms & Fitness</h4>  
-          <h4>{ newList[1].name }</h4>
-        </li>
-      </div>
-    );
+  renderList(list) {
+    var newList = list.slice(0, 3);
+    return newList.map((place) => {
+      return (
+        <div>
+          <li className="placesLI" 
+              key={place.place_id} >
+            <h4>{place.name }</h4>
+          </li>
+        </div>
+      );    
+    });
   }
 
   render() {
+    var parksList = this.props.activeParks;
+    var gymsList = this.props.activeGyms;
     return (
       <div id="placesContainer">
-        <h1>Nearby Amenities</h1>
-        <ul className="AmenetiesList">
-          { this.props.activeGyms.length && this.renderList() }
-        </ul>
+       <h4>Parks</h4> 
+       <ul className='trainList'>{parksList.length && this.renderList(parksList)}</ul> 
+       <h4>Gyms & Fitness</h4> 
+       <ul className='busList'>{gymsList.length && this.renderList(gymsList)}</ul>
       </div>
     ); 
   }
 }
 
-
 let mapStateToProps = (state) => ({
-  activeTrains: state.activeTrains,
-  activeBus: state.activeBus,
   activeParks: state.activeParks,
   activeGyms: state.activeGyms
 });
