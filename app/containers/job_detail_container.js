5c670d71a56d7e3a527cb28c87bc467d0ab88ddf
Damon <damonnguyen92@gmail.com>
2016-08-17 17:24:39 -0700

    Feat(init): Initially display scrap data for the description component

--- app/containers/job_detail_container.js
+++ app/containers/job_detail_container.js
@@ -14,20 +14,23 @@ class JobDetail extends BaseComponent {
     if (!this.props.job) {
       return <h1>Job</h1>;
     }
-    
+    if (this.props.scrapDetails) {
+      var details = this.props.scrapDetails.data.details;
+    }
     return (
       <div className="job-detail">
         <h3><a href={this.props.job.url} target="_new">{ this.props.job.jobtitle }</a></h3>
         <h4>{ this.props.job.company + ', ' + this.props.job.formattedLocation}</h4> 
         <p>{ this.props.job.formattedRelativeTime }</p>
-        <blockquote><a href={this.props.job.url} target="_new">{ this.tagFreeSnippet(this.props.job.snippet) }</a><small>[<a href={this.props.job.url} target="_new">more</a>]</small></blockquote>
+        <blockquote><a href={this.props.job.url} target="_new">{ details }</a><small>[<a href={this.props.job.url} target="_new">more</a>]</small></blockquote>
       </div>
     );
   }
 }
 
 let mapStateToProps = (state) => ({ 
-  job: state.activeJob 
+  job: state.activeJob,
+  scrapDetails: state.scrapDetails
 });
 
 export default connect(mapStateToProps)(JobDetail);
