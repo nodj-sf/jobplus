7954f8ce86f44aee612151defdab8ec5f558b68f
Justin Lien <lien.justin.m@gmail.com>
2016-08-20 11:07:13 -0700

    fix(spacing): add ellipsis and space between more

--- app/containers/job_detail_container.js
+++ app/containers/job_detail_container.js
@@ -22,7 +22,7 @@ class JobDetail extends BaseComponent {
         <h3><a href={this.props.job.url} target="_blank">{ this.props.job.jobtitle }</a></h3>
         <h4>{ this.props.job.company + ', ' + this.props.job.formattedLocation}</h4> 
         <p>{ this.props.job.formattedRelativeTime }</p>
-        <blockquote><a href={this.props.job.url} target="_blank">{ details }</a><small>[<a href={this.props.job.url} target="_new">more</a>]</small></blockquote>
+        <blockquote><a href={this.props.job.url} target="_blank">{ details }</a>&hellip; <small>[<a href={this.props.job.url} target="_new">more</a>]</small></blockquote>
       </div>
     );
   }
