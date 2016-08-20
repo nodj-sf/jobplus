a91f589c2c4f05f3d0e796138567ca561afa6314
Justin Lien <lien.justin.m@gmail.com>
2016-08-20 11:28:42 -0700

    chore(style): remove paragraph link and show hover state

--- app/containers/job_detail_container.js
+++ app/containers/job_detail_container.js
@@ -19,10 +19,10 @@ class JobDetail extends BaseComponent {
     }
     return (
       <div className="job-detail">
-        <h3><a href={this.props.job.url} target="_blank">{ this.props.job.jobtitle }</a></h3>
+        <h3><a href={this.props.job.url} className="link-state" target="_blank">{ this.props.job.jobtitle }</a></h3>
         <h4>{ this.props.job.company + ', ' + this.props.job.formattedLocation}</h4> 
         <p>{ this.props.job.formattedRelativeTime }</p>
-        <blockquote><a href={this.props.job.url} target="_blank">{ details }</a>&hellip; <small>[<a href={this.props.job.url} target="_new">more</a>]</small></blockquote>
+        <blockquote>{ details }&hellip; [<a href={this.props.job.url} className="link-state" target="_new">more</a>]</blockquote>
       </div>
     );
   }
