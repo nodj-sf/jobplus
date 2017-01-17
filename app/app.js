387bcc16a88acbf3f5b7fe30771140fc4eebacb2
Oliver Isenrich <IsenrichO@gmail.com>
2017-01-16 19:28:57 -0800

    chore: Inject styles at top-level component

--- app/app.js
+++ app/app.js
@@ -6,6 +6,17 @@ import { Router, Route, hashHistory, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
 import promiseMiddleware from 'redux-promise';
 
+import '../public/css/normalize.css';
+import '../public/css/skeleton.css';
+
+import '../public/css/styles.css';
+import '../public/css/custom_class_styles.css';
+import '../public/css/jobResults_styles.css';
+import '../public/css/jobDetail_styles.css';
+import '../public/css/fontAwesome_styles.css';
+import '../public/css/mobile_style.css';
+import '../public/css/keyframes.css';
+
 import Results from './components/results_page';
 import LandingPage from './components/landing_page';
 import AboutPage from './components/about_page';
