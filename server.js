eb4b60cc0ea8d9d7d11f68c80d10d937428acfee
Oliver Isenrich <IsenrichO@gmail.com>
2017-01-17 05:36:46 -0800

    config (server): Modify API key names and session tokens

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const app = require('./server/router.js'),
+'use strict';
+
+const App = require('./server/router.js'),
       PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+App.listen(PORT, () => {
   console.log(`Server up running on LocalHost: ${PORT}`);
 });
