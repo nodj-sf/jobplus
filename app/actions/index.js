fef3f736762bf0db1da8dcea1e17c8b1f4883b46
Oliver Isenrich <IsenrichO@gmail.com>
2017-01-17 01:45:02 -0800

    chore: Revert to former getCookie action creator version

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
 
+// const getCookie = (name) => {
+//   const value = `;${document.cookie}`,
+//         parts = value.split('; ' + name + '=');
+//   return parts.length === 2 ? decodeURIComponent(parts.pop().split(';').shift()) : '';
+// };
+
 const getCookie = (name) => {
-  const value = `;${document.cookie}`,
-        parts = value.split('; ' + name + '=');
-  return parts.length === 2 ? decodeURIComponent(parts.pop().split(';').shift()) : '';
+  const value = '; ' + document.cookie;
+  const parts = value.split('; ' + name + '=');
+  if (parts.length === 2) {
+    return decodeURIComponent(parts.pop().split(';').shift());
+  }
+  return '';
 };
 
 export const fetchJobs = (jobSearch, city) => {
