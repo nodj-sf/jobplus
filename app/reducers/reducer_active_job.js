7f4d8cf787b1b5519f1ddb9d31908c6d647e5f83
Oliver Isenrich <IsenrichO@gmail.com>
2017-01-19 00:59:40 -0800

    chore (reducers): Include 'use strict' directive

--- app/reducers/reducer_active_job.js
+++ app/reducers/reducer_active_job.js
@@ -1,8 +1,12 @@
+'use strict';
+import { JOB_SELECTED } from '../actions/index';
+
+
 export default function(state = {}, action) {
   // console.log(`Action ${action} on Active Jobs Reducer.`);
   switch(action.type) {
-    case 'JOB_SELECTED':
-      // console.log(`Active Job Key ID: ${action.payload.jobkey}`);
+    case JOB_SELECTED:
+      console.log(`Queried active job (with key/ID ${action.payload.jobkey}) and payload:\n`, action.payload);
       return action.payload;
     default:
       // console.log(`Unknown action <${action}> executed! Returning fallback output.`);
--- app/reducers/reducer_active_yelp.js
+++ app/reducers/reducer_active_yelp.js
@@ -1,9 +1,13 @@
+'use strict';
+import { FETCH_YELP } from '../actions/index';
+
+
 export default function(state = [], action) {
   switch(action.type) {
-    case 'FETCH_YELP':
+    case FETCH_YELP:
       console.log(`Yelp API Payload:\n`, action.payload.data);
       return action.payload.data;
     default:
       return state;
   }
-}
+};
