905523dc8849a7c87d41d72bc850dc3013d8b49e
Oliver Isenrich <IsenrichO@gmail.com>
2017-01-19 00:57:13 -0800

    chore: Include basic error-handling code

--- app/components/GooglePlacesAutocompleteAPI.js
+++ app/components/GooglePlacesAutocompleteAPI.js
@@ -177,7 +177,7 @@ class GooglePlacesAutocomplete extends Component {
   }
 
   renderSuggestedLocation(place, id) {
-    console.log('LOGGED PLACE:', place);
+    // console.log('LOGGED PLACE:', place);
     return (
       <li
         key={ `AutoSuggestLocation_${place.placeId}` }
