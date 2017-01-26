c1146a4d7a2cd0769aa235c3b31086042a6a312b
Oliver Isenrich <IsenrichO@gmail.com>
2017-01-26 05:27:10 -0800

    chore: Rename parent React component

---
+++ app/App.jsx
@@ -0,0 +1,44 @@
+'use strict';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, hashHistory, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
+import promiseMiddleware from 'redux-promise';
+import Results from './components/ResultsPage';
+import LandingPage from './components/LandingPage';
+import AboutPage from './components/AboutPage';
+import reducers from './reducers/index';
+
+import '../assets/styles/master.scss';
+
+import '../assets/css/normalize.css';
+import '../assets/css/skeleton.css';
+
+import '../assets/css/styles.css';
+import '../assets/css/jobResults_styles.css';
+import '../assets/css/fontAwesome_styles.css';
+
+
+
+const routerMid = routerMiddleware(browserHistory),
+      createStoreWithMiddleware = applyMiddleware(promiseMiddleware, routerMid)(createStore),
+      store = createStoreWithMiddleware(reducers),
+      history = syncHistoryWithStore(browserHistory, store);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <Router history={ history }>
+          <Route path='/'component={ LandingPage } />
+          <Route path='results' component={ Results } />
+          <Route path='about' component={ AboutPage } />
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
