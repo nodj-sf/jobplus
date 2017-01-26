082f870a6a2df100bbdf5b06e0e372a2d9e87dff
Oliver Isenrich <IsenrichO@gmail.com>
2017-01-26 07:29:29 -0800

    init (SVG): Create Job+ inline SVG logo component

---
+++ app/constants/svg/AppLogo_SVG.jsx
@@ -0,0 +1,38 @@
+'use strict';
+import React from 'react';
+
+
+const AppLogo = () => (
+  <svg
+    className="svg-glyphs app-logo-glyph"
+    // width="177px"
+    // height="163px"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="457 250 177 163">
+    <text
+      stroke="none"
+      fill="none"
+      font-family="Oswald-Bold, Oswald"
+      font-size="100"
+      font-weight="500"
+      letter-spacing="1.38888884">
+      <tspan
+        x="457.166546"
+        y="369"
+        fill="#FFF">
+        Job
+      </tspan>
+      <tspan
+        x="591.36251"
+        y="369"
+        font-size="110"
+        letter-spacing="1.54320979"
+        fill="#7A7A7A">
+        +
+      </tspan>
+    </text>
+  </svg>
+);
+
+export default AppLogo;
