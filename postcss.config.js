14409d223136ed7b289c5e376ef87b97db9422a7
Oliver Isenrich <IsenrichO@gmail.com>
2017-01-26 18:38:12 -0800

    chore: Overwrite with existing PersonalRepo

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  syntax: 'postcss-scss',
-  plugins: [
-    require('autoprefixer'),
-    require('postcss-import'),
-    require('postcss-nested')
-  ]
-};
--- server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const App = require('./server/router.js'),
-      PORT = process.env.PORT || 3000;
-
-App.listen(PORT, () => {
-  console.log(`Server up running on LocalHost: ${PORT}`);
-});
